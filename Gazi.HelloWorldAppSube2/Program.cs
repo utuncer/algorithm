762e791290cfdd34e24bd666e4e5f9b86f30e229
Umut Tunçer <utuncerx@gmail.com>
2024-12-01 21:36:46 +0300

    Hocanın Son Projesi 10. Hafta

--- Gazi.HelloWorldAppSube2/Program.cs
+++ Gazi.HelloWorldAppSube2/Program.cs
@@ -1,250 +1,236 @@
 ﻿using System;
 
 namespace Gazi.HelloWorldAppSube2
-
 {
     internal class Program
     {
+        static int sayi = 15;
         static void Main(string[] args)
         {
-            #region Console.WriteLine’da Yazım (Syntax) Kuralları - String Birleştirme
-            //string isim = "ahmet";
-            //string soyad = "mehmet";
+            #region Veri Tipleri ve Tanımlamaları
+            //Console.WriteLine("Gazi");
+            //Console.WriteLine("Üniversitesi");
 
-            //Console.WriteLine("hoşgeldiniz, " + isim + " " + soyad);
-            //Console.WriteLine("hoşgeldiniz, {0} {1}", isim, soyad);
-            //Console.WriteLine($"hoşgeldiniz, {isim} {soyad}");
+            //const byte a = 20;
+            //double d;
+            //a = 30;
+            //d = 40;
+            //Console.Write(a);
+            //byte a = 30;
+            //double d = 40;
+            //a = 50;
+
+            //var number = 10;
+
+            //Console.WriteLine(a);
+
+            //const int a = 50;
+
+            //byte yas;
+            //Console.WriteLine(yas);
+
+            //int sayi = 10;
+            //byte number = 10;
+
+            //float sayi = 3.15f;
+            //double _sayi = 3.15;
+            //decimal __sayi = 3.15m;
+            //var sayi = 10.5D;//Implicit (Veri tipi belirlenmeden yapılan tanılamalar
+            //byte number = 5;//Explicit (Veri tipinin açık olara belirtildiği tanımlamalar)
+            //var sonuc = true; 
+            #endregion
+
+            #region String Birleştime İşlemleri
+            //string isim = "Ahmet";
+            //string soyad = "Mehmet";
+
+            //Console.WriteLine("Hoşgeldin, "+ isim + " " + soyad);
+            //Console.WriteLine("Hoşgeldin, {0} {1}", isim, soyad);
+            //Console.WriteLine($"Hoşgeldin, {isim} {soyad}");
             #endregion
 
             #region Kullanıcı Etkileşimleri
-            //Console.Write("isminizi giriniz: ");
-            //string name = Console.ReadLine();
-            //Console.Write("soyadınızı giriniz: ");
-            //string soyad = Console.ReadLine();
-            //Console.Write("yaşınızı giriniz: ");
-            //string yas = Console.ReadLine();
-            //Console.WriteLine($"hoşgeldin,\n{name} {soyad}\nyaşınız {yas}");
+            //try
+            //{
+            //    Console.Write("İsminizi Giriniz:");
+            //    string name = Console.ReadLine();
+            //    Console.Write("Soyadınızı giriniz:");
+            //    string surname = Console.ReadLine();
+            //    Console.Write("Yaşınızı giriniz:");
+            //    byte age = byte.Parse(Console.ReadLine());
+            //    Console.WriteLine($"Hoşgeldiniz,\nİsminiz:{name}\nSoyadınız:{surname}\nYaşınız:{age}");
+            //}
+            //catch (FormatException)
+            //{
+            //    Console.WriteLine("Yaşınızı sayı ile giriniz.");
+            //}
+            //catch (OverflowException)
+            //{
+            //    Console.WriteLine("0-255 arası değer giriniz");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Bir hata oluştu...");//Kullanıcıya Göster
+
+            //    Console.WriteLine($"Tarih:{DateTime.Now}\nMessage:{ex.Message}\nStack Trace:{ex.StackTrace}");//Kendimiz için tuttuğumuz loglar.Veritabanı,txt dosyası,email vb olabilir.
+            //}
+
             #endregion
 
-            #region Tür Dönüşümleri
-            //byte sayi1 = 20;
-            //int number1 = sayi1; // Implicit Type Casting
+            //double maas = 5000;
+            //maas -= 1000;// maas = maas - 1000;
+            //Console.WriteLine(maas);
 
-            //checked // Veri kaybı ihtimali varsa hata atama yapar.
+            //int sayi1 = 10;
+            //int sayi2 = 20;
+
+            //int sayi3 = ++sayi1;
+
+
+            // Console.WriteLine(sayi2 % sayi1);
+
+            //Ternary Operator - 3 adet operand alır.
+            //string sonuc = sayi1 > sayi2 ? "Sayı 1 büyük" : "Sayı2 büyük veya sayılar eşit";
+
+            #region Tür Dönüşümleri
+
+            //byte sayi = 20;
+            //int number = sayi;//Implicit Type Casting
+
+            //checked
             //{
-            //    int sayi2 = 256;
-            //    byte number2 = (byte)sayi2; //explicit Type Casting
-            //    Console.WriteLine(number2);
+            //    int sayi = 257;
+            //    byte number = (byte)sayi;//Explicit Type Casting
+            //    Console.WriteLine(number);
             //}
 
             //string value = "20";
             //string value2 = "30";
-            //byte sayi3 = Convert.ToByte(value);
-            //byte sayi4 = byte.Parse(value2);
-            //Console.WriteLine(sayi3 + sayi4);
+            //byte sayi = Convert.ToByte(value);
+            //byte sayi2 = byte.Parse(value2);
+            //Console.WriteLine(sayi + sayi2);
 
-            //int sayi5 = 20;
-            //object value5 = sayi5; //boxin
-            //int number5 = (int)value5; //unboxing
-
-            //try
-            //{
-            //    Console.Write("Adınızı Girin: ");
-            //    string name = Console.ReadLine();
-            //    Console.Write("Yaşınızı Girin: ");
-            //    byte age = Convert.ToByte(Console.ReadLine());
-            //}
-            //catch (FormatException)
-            //{
-            //    Console.WriteLine("Yaşınızı sayılar ile girin...");
-            //}
-            //catch (OverflowException)
-            //{
-            //    Console.WriteLine("Yaşınız 0 ile 255 arasında olmalı...");
-            //}
-            //catch(Exception ex)
-            //{
-            //    Console.WriteLine("Bir hata oluştu..."); //Kullanıcıya göster
-
-            //    Console.WriteLine($"Tarih: {DateTime.Now}\nMessage: {ex.Message}\nStack Trace: {ex.StackTrace}"); // Kendimiz için tuttuğumuz loglar.Veritabanı, txt dosyası, email vb olabilir.
-            //}
-
-            //int sayi1 = 10;
-            //int sayi2 = sayi1++;// sayi2 = 10
-            //int sayi3 = ++sayi1;//sayi3 = 11
-
-            //int sayi1 = 10;
-            //int sayi2 = 20;
-            //string sonuc = sayi1 > sayi2 ? "Sayı 1 büyük" : "Sayı 2 büyük veya sayılar eşit"; //Ternary Operator - 3 adet operand (işleme giren değer) alır
-            //Console.WriteLine(sonuc);
-
+            //int sayi = 20;//STACK
+            //object value = sayi;//STACK->HEAP Boxing
+            //int number = (int)value;//Heap->Stack Unboxing 
             #endregion
 
-            #region F5 ile ve CTRL +F5 ile projeyi başlatmanın farkı
-            // F5 ile başlatıldığında
-            /*
-             Hata Ayıklama Modunda Başlatır (Debugging Mode).
-Uygulama hata ayıklayıcı (debugger) ile çalıştırılır.
-             */
-            // CTRL + F5 ile başlatıldığında
-            /*
-             Hata Ayıklama Modu Olmadan Çalıştırır (Run Without Debugging).
-Uygulama, hata ayıklayıcı devre dışı bırakılarak doğrudan çalıştırılır.
-             */
-            #endregion
 
-            #region 1. ve 2. sayıyı karşılaştıran if kodu
+            //Console.WriteLine("1. sayıyı giriniz:");
+            //int sayi1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("2. sayıyı giriniz:");
+            //int sayi2 = int.Parse(Console.ReadLine());
 
-            //Console.Write("1. Sayıyı Giriniz: ");
-            //int sayi1 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("2. Sayıyı Giriniz: ");
-            //int sayi2 = Convert.ToInt32(Console.ReadLine());
-            //if (sayi1 > sayi2)
+            //if (sayi1>sayi2)
             //{
-            //    Console.WriteLine($"Girdiğiniz ilk sayi ikinci sayıdan büyük {sayi1} > {sayi2}");
+            //    Console.WriteLine("Sayı 1 büyüktür");
             //}
-            //else if (sayi1 < sayi2)
+            //else if(sayi2>sayi1)
             //{
-            //     Console.WriteLine($"Girdiğiniz ilk sayi ikinci sayıdan küçük {sayi1} < {sayi2}");
+            //    Console.WriteLine("Sayı 2 büyüktür");
             //}
             //else
             //{
-            //    Console.WriteLine($"Girdiğiniz ilk sayi ve ikinci eşit {sayi1} = {sayi2}");
+            //    Console.WriteLine("Sayılar eşit");
             //}
-            #endregion
 
-            #region Hava Durumuna göre yönlendiren if kodu
-            Console.WriteLine("Hava nasıl? (Güzel/Kötü)");
-            string havakontrol = Console.ReadLine().ToLower().Trim();
-           
-            if (havakontrol == "güzel")
+
+            Console.WriteLine("Hava nasıl?(Güzel/Kötü)");
+            string cevap = Console.ReadLine().ToLower().Trim();
+
+            if (cevap == "güzel")
             {
-                Console.WriteLine("Dışarı çık eğlen oley");
+                Console.WriteLine("Dışarı çık");
             }
-            else if (havakontrol == "kötü")
+            else if (cevap == "kötü")
             {
-                Console.WriteLine("Dışarı çıkma otur evinde");
+                Console.WriteLine("Evde otur");
             }
             else
             {
-                Console.WriteLine("Güzel veya Kötü'den birini seçmelisiniz");
+                Console.WriteLine("Sadece Güzel/Kötü cevabı verebilirsiniz.");
             }
-            #endregion
-
+            Console.ReadKey();
         }
     }
 }
 
-/*
+//namespace:İçinde classları bulunduran yapılardır.
+//Scope(Blok Yapıları): {} parantezleri arasındaki alanlardır.
+//Class: İçinde metodları bulunduran yapılardır.(Bu tanım şimdilik yüzeysel olarak yapılmıştır. Daha sonra detaylandırılacaktır)
+//Metod: İş yapan kod bloklarına metod denir.
+//Main metodu: Console uygulamalarının başlangıç noktasıdır.
+//Ctrl+F5: Uygulamayı hata ayıklama modu olmadan başlatır.
+
+//Syntax:Kod yazım kuralları
+//1-Açılan her parantez mutlaka kapatılmalıdır.
+//2- Scope tanımlamaları sonunda ; kullanılmaz ancak komut satırları sonlarında kullanılır.
+
+//Syntax Hatalarını Nasıl Anlarız?
+//1- Hata olan kodun altı kırmızı ile çizilir
+//2-Scrool bar'da hata olan yere kırmızı işaretleme yapılır
+//3-VS kod editörünün alt çubuğunda Kırmızı renkli X ve yanında hata sayısı görülür.
+
+//Syntax Hatası olan program çalışır mı? Hayır, çünkü program derlenmez.
+
+//IntelliSense: Kod yazarken açılan pencere ile karşımıza çıkan öneriler
+//IntelliCode:Yapay zeka destekli kod yazım yardımcısı. Gelen önerileri TAB tuşu ile kabul edebiliriz.
+
+//Solution->Project(s)
+//View->Solution Explorer
+
+//Ctrl+K+D: Kod hiyerarşisini düzenlemek için kullanılır.
+//Ctrl+K+C: Seçili satırları açıklama satırı haline dönüştürür
+//Ctrl+K+U: Açıklama satırını tekrar normal satır haline dönüştürür
 
 
-namespace: İçinde classları bulunduran yapılardır.
+//C# -> Derleme -> MS-IL -> .NET Runtime(Derleme) -> 0-1(Binary)
 
-Scope(Blok Yapıları): {} parantezleri arasındaki alanlardır.
+//Microsoft Intermediate Language
 
-Class: İçinde metodları bulunduran yapılardır. (Bu tanım şimdilik yüzeysel 
-olarak yapılmıştır. Daha sonra detaylandırılacaktır)
+//1 Bit = 0 veya 1
+//1 byte=8bit
+//1 Kilo Byte(KB)=1024 byte
+//1 Megabyte(MB) = 1024KB
+//1 Gigabyte(GB) = 1024MB
+//1 Terabyte(TB) = 1024GB
 
-Metod: İş yapan kod bloklarına metod denir.
+//sbyte=Signed Byte
+//ushort=Unsigned Short
 
-Main Metodu: Console uygulamalarının başlangıç noktasıdır.
+//Değer tipleri belleğin STACK bölgesini kullanır
+//Referans tipleri HEAP bölgesi kullanır
+//HEAP bölgesi stack bölgesine göre daha büyüktür. Dolayısıyla tek seferde birden fazla veri depolayabilir. Örn. Ahmet stringinde 5 tane char vardır.
+//Referans tipi değişkenlerde, referanslar STACK bölgesinde, değerler HEAP bölgesinde tutulur. Değere ulaşmak için, o değerin HEAP bölgesindeki adresini bilen referansına ulaşmak gereklidir.
 
-Ctrl + F5: Uygulamayı hata ayıklama modu olmadan başlatır.
-    
-Syntax: Kod yazım kuralları
+//Metod Parametresi: Metodların işlerini yapabilmek için ihtiyaç dudukları verilerdir. Bir metod hiç parametre istemeyebilir yada birden fazla veri tipinde paramere de kabul edebilir.
 
-1- Açılan her parantez mutlaka kapatılmalıdır
-2- Scope tanımlamaları sonunda ; kullanılmaz ancak komut satırları sonlarında kullanılır.
+//void: Bu geri dönüş tipine sahip metodlar, işlerini yaptıktan sonra geriye bilgi dönmezler.
 
-Syntax hatalarını nasıl anlarız?
-1-Hata olan kodu altı kırmızı ile çizilir
-2-Scrool bar'da hata oaln yere kırmızı işaretleme yapılır
-3-VS (Visual Studio) kod editörünün alt çubuğunda kırmızı renkli x ve yanında hata sayısı görülür
+//C# programlama dili Tip Güvenliği(Type Safety) sağlayan bir dildir.
+//Uyumsuz tipler arası veri ataması yapılamaz. Örn. string->byte
+//checked blokları: Büyük türden küçük türe yapılan dönüşümlerde hata atarak, veri kaybı ihtimalini önler.
+//Debug:Hata yakalama işlemleri. 
+//Hata yönetimleri C#'da try-catch ile yapılır
 
-Syntax Hatası olan program çalışır mı? Hayır, son başarılı derlemeyi çalıştırma seçeneği karşımıza çıkar
+//Ctrl+F5: Start without debugging
+//F5: Start Debugging
 
-Uzun kodları alt satıra geçirme
-tools -> options -> text editor -> c# -> Word Wrap
+//1 && 1 = 1
+//1 && 0 = 0
+//0 && 1 = 0
+//0 && 0 = 0
 
-System.Console.WriteLine
-nameplace.class.metod
+//1 || 1 = 1
+//1 || 0 = 1
+//0 || 1 = 1
+//0 || 0 = 0
 
-Yazı önerisi çıkarma kısa yolu
-Ctrl + Space
+//Breakpoint:Programın istenen satırda durdurulup, sonraki satırların adım adım izlenmesine olanak sağlayan DEBUG yöntemidir.
 
-IntelliSense: Kod yazarken açılan pencere ile karşımıza çıkan öneriler
+//Case Sensitive: C# programlama dilinde büyüm harfler ile küçük harfler aynı değildir. Büyük/Küçük harf duyarlılığı
 
-IntelliCode: Yapay zeka destekli kod yazım yardımcısı. Gelen önerileri TAB tuşu ile kabul edebiliriz.
-
-Solution -> Project(s) ->
-
-Solution Explorer penceresi kapandığında
-View -> Solution Explorer
-
-Çalışma dosyasının nerede olduğunu görmek için
-
-Solutin "[projeismi]" sağ tık -> Open Folder in File Explorer
-
-.sln uzantılı dosya çalışma ortamını tekrar açar
-
-kodların yapısını boşluklarını ..vb düzeltmek için Ctrl K + Ctrl D
-
-Açıklama satırı için tek satıra // paragraft 
-
-Ctrl K + Ctrl C satırı yorum satırı yapar
-
-Ctrl K + Ctrl U yorum satırını normal satır haline getirir
-
-Ctrl B derlemeninn başarılı mı başarısız mı olduğunu kontro eder
-
-C# -> Derleme -> MS-IL (ilk derleme makine dili değildir)
-
-MS-IL : Microsoft Intermadiate Language
-
-C# -> Derleme -> MS-IL -> .Net Runtime (Derleme) -> 0-1(Binary)
-
-Farklı işlemcilerde de çaluşmasıı için MS-IL vardır
-MS-IL olmasaydı program yazıldığında işlemciye özel derlendiğinden başka sistemlerde kullanılırken hata olurdu
-
-Değişkenleri tek satırda da tanımlayabilirsin
-byte a = 30, b;
-
-**Static Değişken Tanımlamak**
-
-Tanımlamanın başına sadece static sözcüğü getirilir.
-
-static byte a;
-
-a ? 30;
-
-**Sabit Değişken Tanımlamak**
-
-Tanımlamanın başına sadece const sözcüğü getirilir.
-
-Değişkenin değeri sonradan değiştirilemez.
-
-const byte a;
-
-a = 30; x
-
-const byte a = 30;
-
-int* yas;
-
-- HEAP bölgesi STACK bölgesine göre daha büyüktür. Dolayısıyla tek seferde birden fazla veri depolayabilir. Örn: Ahmet stringinde 5 tane char vardır.
-- Referans tipi değişkenlerde, referanslar STACK bölgesinde, değerler HEAP bölgesinde tutulur. Değere ulaşmak için, o değerin HEAP bölgesindeki adresini bilen referansına ulaşmak gereklidir.
-- **Metod Parametresi:** Metodların işlerini yapabilmek için ihtiyaç duydukları verilerdir. Bir metod hiç parametre istemeyebilir yada birden fazla veri tipinde parametre de kabul edilebilir.
-
-
-Void: Bu geri dönüş tipine sahip metodlar, işlerini yaptıktan sonra geriye bilgi dönmezler. 
-
-Tip Güvenliği: Mesela elinde byte değişkeni olsun string değer ataması yapılamayacağından stringi byte'a dönüştürmek gerekli. 
-
-Case Sensitive: C# programlama dilinde büyük harfler ile küçük harfler aynı değildir.
-
-String classının metodlarından bazılar
-ToLower(): Tüm karakterleri küçük harfe çevir
-ToUpper(): Tüm karakterleri büyük harfe çevir
-Trim(): Tüm karakterleri büyük harfe çevir
- */
-
+//String classının metodlarından bazıları
+//ToLower(): Tüm karakterleri küçük harfe çevir
+//ToUpper():Tüm karakterleri büyük harfe çevir
+//Trim(): String ifadenin başında ve sonunda bulunan boşlukları siler
