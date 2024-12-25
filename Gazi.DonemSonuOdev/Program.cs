2feb27e84d837329427f9547ff115638f3934b33
Umut Tunçer <utuncerx@gmail.com>
2024-12-26 00:28:38 +0300

    14. Hafta Dönem Sonu Ödevi v3

--- Gazi.DonemSonuOdev/Program.cs
+++ Gazi.DonemSonuOdev/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Gazi.DonemSonuOdev
+﻿using System.ComponentModel;
+
+namespace Gazi.DonemSonuOdev
 {
     internal class Program
     {
@@ -16,26 +18,36 @@
             double enKucuk = 0;
             double enBuyuk = 0;
 
+        string[] tabloBaslik = new string[]
+        {
+                    "Numara",
+                    "Ad",
+                    "Soyad",
+                    "Vize Notu",
+                    "Final Notu",
+                    "Ortalama",
+                    "Harf Notu"
+        };
+
             try
             {
                 Console.Write("Kaç öğrenci kaydetmek istiyorsunuz: ");
                 int ogrenciSayisi = Convert.ToInt32(Console.ReadLine());
 
 
-                double[] ortalamaToplam = new double[ogrenciSayisi];
+        double[] ortalamaToplam = new double[ogrenciSayisi];
 
 
 
-                string[,] dizi = new string[ogrenciSayisi + 1, 7];
-                dizi[0, 0] = "Numara";
-                dizi[0, 1] = "Ad";
-                dizi[0, 2] = "Soyad";
-                dizi[0, 3] = "Vize Notu";
-                dizi[0, 4] = "Final Notu";
-                dizi[0, 5] = "Ortalama";
-                dizi[0, 6] = "Harf Notu";
+        string[,] dizi = new string[ogrenciSayisi + 1, tabloBaslik.Length];
 
-                for (int i = 0; i < ogrenciSayisi; i++)
+                for (int i = 0; i<tabloBaslik.Length; i++)
+                {
+                    dizi[0, i] = tabloBaslik[i];
+                }
+
+
+                for (int i = 0; i<ogrenciSayisi; i++)
                 {
                     Console.Write($"{i + 1}. Öğrencinin Numarasını Giriniz: ");
                     dizi[i + 1, 0] = Console.ReadLine();
@@ -54,19 +66,19 @@
                             dizi[i + 1, 3] = Console.ReadLine();
                             vizeNotu = Convert.ToDouble(dizi[i + 1, 3]);
 
-                            if (vizeNotu > 100 || vizeNotu < 0)
+                            if (vizeNotu > 100 || vizeNotu< 0)
                             {
                                 Console.WriteLine(notDegeriHata);
                             }
                             else
-                            {
-                                break;
-                            }
+{
+    break;
+}
                         }
                         catch (FormatException)
                         {
-                            Console.WriteLine(formatHata);
-                        }
+    Console.WriteLine(formatHata);
+}
                         catch (Exception hata)
                         {
                             Console.WriteLine($"Beklenmeyen bir hata oluştu: {hata.Message}");
@@ -74,72 +86,72 @@
                     }
 
                     while (true)
-                    {
-                        try
-                        {
-                            Console.Write($"{i + 1}. Öğrencinin Final Notunu Giriniz: ");
-                            dizi[i + 1, 4] = Console.ReadLine();
-                            finalNotu = Convert.ToDouble(dizi[i + 1, 4]);
+{
+    try
+    {
+        Console.Write($"{i + 1}. Öğrencinin Final Notunu Giriniz: ");
+        dizi[i + 1, 4] = Console.ReadLine();
+        finalNotu = Convert.ToDouble(dizi[i + 1, 4]);
 
-                            if (finalNotu > 100 || finalNotu < 0)
-                            {
-                                Console.WriteLine(notDegeriHata);
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                        catch (FormatException)
-                        {
-                            Console.WriteLine(formatHata);
-                        }
-                        catch (Exception hata)
-                        {
-                            Console.WriteLine($"Beklenmeyen bir hata oluştu: {hata.Message}");
-                        }
-                    }
+        if (finalNotu > 100 || finalNotu < 0)
+        {
+            Console.WriteLine(notDegeriHata);
+        }
+        else
+        {
+            break;
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine(formatHata);
+    }
+    catch (Exception hata)
+    {
+        Console.WriteLine($"Beklenmeyen bir hata oluştu: {hata.Message}");
+    }
+}
 
-                    notOrtalama = vizeNotu * 0.40 + finalNotu * 0.60;
-                    dizi[i + 1, 5] = notOrtalama.ToString();
-                    ortalamaToplam[i] = notOrtalama;
+notOrtalama = vizeNotu * 0.40 + finalNotu * 0.60;
+dizi[i + 1, 5] = notOrtalama.ToString();
+ortalamaToplam[i] = notOrtalama;
 
-                    if (notOrtalama >= 85) { dizi[i + 1, 6] = "AA"; }
-                    else if (notOrtalama >= 75) { dizi[i + 1, 6] = "BA"; }
-                    else if (notOrtalama >= 60) { dizi[i + 1, 6] = "BB"; }
-                    else if (notOrtalama >= 50) { dizi[i + 1, 6] = "CB"; }
-                    else if (notOrtalama >= 40) { dizi[i + 1, 6] = "CC"; }
-                    else if (notOrtalama >= 30) { dizi[i + 1, 6] = "DC"; }
-                    else if (notOrtalama >= 20) { dizi[i + 1, 6] = "DD"; }
-                    else if (notOrtalama >= 10) { dizi[i + 1, 6] = "FD"; }
-                    else { dizi[i + 1, 6] = "FF"; }
+if (notOrtalama >= 85) { dizi[i + 1, 6] = "AA"; }
+else if (notOrtalama >= 75) { dizi[i + 1, 6] = "BA"; }
+else if (notOrtalama >= 60) { dizi[i + 1, 6] = "BB"; }
+else if (notOrtalama >= 50) { dizi[i + 1, 6] = "CB"; }
+else if (notOrtalama >= 40) { dizi[i + 1, 6] = "CC"; }
+else if (notOrtalama >= 30) { dizi[i + 1, 6] = "DC"; }
+else if (notOrtalama >= 20) { dizi[i + 1, 6] = "DD"; }
+else if (notOrtalama >= 10) { dizi[i + 1, 6] = "FD"; }
+else { dizi[i + 1, 6] = "FF"; }
 
-                    Console.WriteLine(" ");
+Console.WriteLine(" ");
                 }
 
                 enKucuk = ortalamaToplam.Min();
-                enBuyuk = ortalamaToplam.Max();
-                sinifNotOrtalama = ortalamaToplam.Average();
+enBuyuk = ortalamaToplam.Max();
+sinifNotOrtalama = ortalamaToplam.Average();
 
-                for (int i = 0; i < ogrenciSayisi + 1; i++)
-                {
-                    for (int j = 0; j < 7; j++)
-                    {
-                        Console.Write(dizi[i, j] + " ");
-                    }
-                    Console.WriteLine(" ");
-                }
+for (int i = 0; i < ogrenciSayisi + 1; i++)
+{
+    for (int j = 0; j < tabloBaslik.Length; j++)
+    {
+        Console.Write(dizi[i, j] + " ");
+    }
+    Console.WriteLine(" ");
+}
 
-                Console.WriteLine($"\nSınıf Ortalaması: {sinifNotOrtalama:F2}\nEn Düşük Not: {enKucuk}\nEn Yüksek Not: {enBuyuk}");
+Console.WriteLine($"\nSınıf Ortalaması: {sinifNotOrtalama}\nEn Düşük Not: {enKucuk}\nEn Yüksek Not: {enBuyuk}");
             }
             catch (FormatException)
             {
-                Console.WriteLine(formatHata);
-            }
+    Console.WriteLine(formatHata);
+}
             catch (OverflowException)
             {
-                Console.WriteLine(OverflowHata);
-            }
+    Console.WriteLine(OverflowHata);
+}
             catch (Exception hata)
             {
                 Console.WriteLine($"Beklenmeyen bir hata oluştu: {hata.Message}");
